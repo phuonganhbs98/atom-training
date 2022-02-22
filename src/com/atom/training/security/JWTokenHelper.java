a2dd7d5f1019ff22f66392345020016eb0f05ecd
アイン <phuonganhbs1312@gmail.com>
2022-02-22 16:57:37 +0700

    last commit

--- src/com/atom/training/security/JWTokenHelper.java
+++ src/com/atom/training/security/JWTokenHelper.java
@@ -17,7 +17,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
 public class JWTokenHelper {
-	private static final long EXPIRATION_LIMIT_IN_MINUTES = 30;
+	private static final long EXPIRATION_LIMIT_IN_MINUTES = 120;
 	private static final SignatureAlgorithm SIGNATURE_ALGORITHM = SignatureAlgorithm.HS256;
 
 	private static final String SECRET_KEY = "pa-com-atom-training-android-secret-key-test-android-secret-key-test";
@@ -27,26 +27,21 @@ public class JWTokenHelper {
 	}
 
 	public static String createJWT(User user) {
-		// Get the current time
 		long currentTimeInMillis = System.currentTimeMillis();
 		Date now = new Date(currentTimeInMillis);
-		// The privateKey is only valid for the next EXPIRATION_LIMIT_IN_MINUTES
 		long expirationTimeInMilliSeconds = TimeUnit.MINUTES.toMillis(EXPIRATION_LIMIT_IN_MINUTES);
 		Date expirationDate = new Date(currentTimeInMillis + expirationTimeInMilliSeconds);
-		// Will sign our JWT with our ApiKey secret
 		byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY);
 		Key signingKey = new SecretKeySpec(apiKeySecretBytes, SIGNATURE_ALGORITHM.getJcaName());
-		// Sets the JWT Claims sub (subject) value
 		Claims claims = Jwts.claims().setSubject(user.getUserId());
 		claims.put("roles", user.getAdmin());
-		// Let's set the JWT Claims
-		JwtBuilder builder = Jwts.builder() // Configured and then used to create JWT compact serialized strings
-				.setClaims(claims).setId(UUID.randomUUID().toString()) // Sets the JWT Claims jti (JWT ID) value
-				.setIssuedAt(now) // Sets the JWT Claims iat (issued at) value
-				.setIssuer("PA") // Sets the JWT Claims iss (issuer) value
-				.setExpiration(expirationDate) // Sets the JWT Claims exp (expiration) value
+		JwtBuilder builder = Jwts.builder()
+				.setClaims(claims).setId(UUID.randomUUID().toString())
+				.setIssuedAt(now)
+				.setIssuer("PA")
+				.setExpiration(expirationDate)
 				.signWith(signingKey, SIGNATURE_ALGORITHM);
-		// Builds the JWT and serializes it to a compact, URL-safe string
+
 		return builder.compact();
 	}
 
@@ -79,11 +74,10 @@ public class JWTokenHelper {
 	}
 
 	private static Claims decodeJWT(String jwt) {
-		// This line will throw an exception if it is not a signed JWS (as expected)
-		return Jwts.parser() // Configured and then used to parse JWT strings
-				.setSigningKey(DatatypeConverter.parseBase64Binary(SECRET_KEY)) // Sets the signing key used to verify
-				// any discovered JWS digital signature
-				.parseClaimsJws(jwt) // Parses the specified compact serialized JWS string based
+
+		return Jwts.parser()
+				.setSigningKey(DatatypeConverter.parseBase64Binary(SECRET_KEY))
+				.parseClaimsJws(jwt)
 				.getBody();
 	}
 
